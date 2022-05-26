8e6758e48db5e12912f8f3267ab01f6467d91cb2
pavelevandrei <32262415+pavelevandrei@users.noreply.github.com>
2022-05-26 19:22:35 +0300

    Адаптирована очистка ссылок для нескольких файлов

--- source/clean_urls.py
+++ source/clean_urls.py
@@ -1,13 +1,21 @@
 import os
 import pandas as pd
 
-file_name = "78815338_post_attachment.csv"
+owner_id_df = pd.read_csv(os.path.join("..", "data", "owner_ids.csv"))
+owner_ids = owner_id_df["owner_id"].tolist()
 
-path = os.path.join("..", "data", "raw_urls", file_name)
-cleaned_path = os.path.join("..", "data", "clean_urls", file_name)
+print(owner_ids)
+print(len(owner_ids))
 
-df = pd.read_csv(path)
-df = df["Data"].str.split("\"", expand=True)  # будет в третьей колонке
-#print(df[3])
-df.rename(columns={3: "url"}, inplace=True)
-df["url"].to_csv(cleaned_path, index=False)
+for owner_id in owner_ids:
+    file_name = f"{owner_id}_post_attachment.csv"
+
+    path = os.path.join("..", "data", "raw_urls", file_name)
+    cleaned_path = os.path.join("..", "data", "clean_urls", file_name)
+
+    df = pd.read_csv(path)
+    df = df["Data"].str.split("\"", expand=True)  # будет в третьей колонке
+    # print(df[3])
+    df.rename(columns={3: "url"}, inplace=True)
+    df["url"].to_csv(cleaned_path, index=False)
+
