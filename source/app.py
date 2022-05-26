91d341f2585a0ee0b859ded2f9f276609f76945e
pavelevandrei <32262415+pavelevandrei@users.noreply.github.com>
2022-05-26 19:33:04 +0300

    Закачка фото из всех файлов

--- source/app.py
+++ source/app.py
@@ -11,21 +11,29 @@ embedding_pipeline = pipeline('image-embedding')
 # print(type(embedding_pipeline))
 # print(embedding)
 
-owner_id = "78815338"
-file_name = f"{owner_id}_post_attachment.csv"
+owner_id_df = pd.read_csv(os.path.join("..", "data", "owner_ids.csv"))
+owner_ids = owner_id_df["owner_id"].tolist()
 
-url_path = os.path.join("..", "data", "clean_urls", file_name)
-df = pd.read_csv(url_path)
-urls = df["url"].tolist()
-#print(urls)
+url_list = []
+
+for owner_id in owner_ids:
+    file_name = f"{owner_id}_post_attachment.csv"
+
+    url_path = os.path.join("..", "data", "clean_urls", file_name)
+    df = pd.read_csv(url_path)
+    urls = df["url"].tolist()
+    url_list.extend(urls)
+
+print(len(url_list))
 
 embedding_list = []
 for i, url in enumerate(urls[:]):
     print(f"Обрабатывает {i} изображение")
-    urllib.request.urlretrieve(url, os.path.join("..", "data", "images", owner_id, f"{i}.jpg"))
+    urllib.request.urlretrieve(url, os.path.join("..", "data", "images", "all", f"{i}.jpg"))
     embedding = embedding_pipeline(url)
     embedding_list.append(embedding)
-    sleep(0.4)
+    sleep(0.01)
+
 
 emb_path = os.path.join("..", "data", "embeddings", f"{owner_id}.pickle")
 with open(emb_path, 'wb') as file:
