3822ea2c2291c2a69baf1e0ab966b5aa2cb665ff
pavelevandrei <32262415+pavelevandrei@users.noreply.github.com>
2022-05-27 12:03:17 +0300

    Отделение логики расчёта эмбеддингов и части скрипта

--- source/calculate_and_load_emb.py
+++ source/calculate_and_load_emb.py
@@ -16,36 +16,37 @@ class EmbeddingCalculator():
         return self.embedding_pipeline(url)
 
 
-ec = EmbeddingCalculator()
+if __name__ == '__main__':
+    ec = EmbeddingCalculator()
+
+    owner_id_df = pd.read_csv(os.path.join("..", "data", "owner_ids.csv"))
+    owner_ids = owner_id_df["owner_id"].tolist()
+
+    url_list = []
+
+    for owner_id in owner_ids[:]:
+        file_name = f"{owner_id}_post_attachment.csv"
+
+        url_path = os.path.join("..", "data", "clean_urls", file_name)
+        df = pd.read_csv(url_path)
+        urls = df["url"].tolist()
+        url_list.extend(urls)
+
+    print(len(url_list))
+
+    embedding_list = []
+    for i, url in enumerate(url_list[:]):
+        print(f"Обрабатывает {i} изображение")
+        urllib.request.urlretrieve(url, os.path.join("..", "data", "images", "all", f"{i}.jpg"))
+        embedding = ec.calculate_emb(url)
+        embedding_list.append(embedding)
+        sleep(0.01)
+
+    # emb_path = os.path.join("..", "data", "embeddings", f"{owner_id}.pickle")
+    emb_path = os.path.join("..", "data", "embeddings", f"all.pickle")
+    with open(emb_path, 'wb') as file:
+        pickle.dump(embedding_list, file)
+
+    # print(embedding_list)
 
 
-owner_id_df = pd.read_csv(os.path.join("..", "data", "owner_ids.csv"))
-owner_ids = owner_id_df["owner_id"].tolist()
-
-url_list = []
-
-for owner_id in owner_ids[:]:
-    file_name = f"{owner_id}_post_attachment.csv"
-
-    url_path = os.path.join("..", "data", "clean_urls", file_name)
-    df = pd.read_csv(url_path)
-    urls = df["url"].tolist()
-    url_list.extend(urls)
-
-print(len(url_list))
-
-embedding_list = []
-for i, url in enumerate(url_list[:]):
-    print(f"Обрабатывает {i} изображение")
-    urllib.request.urlretrieve(url, os.path.join("..", "data", "images", "all", f"{i}.jpg"))
-    embedding = ec.calculate_emb(url)
-    embedding_list.append(embedding)
-    sleep(0.01)
-
-
-#emb_path = os.path.join("..", "data", "embeddings", f"{owner_id}.pickle")
-emb_path = os.path.join("..", "data", "embeddings", f"all.pickle")
-with open(emb_path, 'wb') as file:
-    pickle.dump(embedding_list, file)
-
-#print(embedding_list)
