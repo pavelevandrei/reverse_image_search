8bcb2552e8fe04c87dfeeb717a6f753e0b06779b
pavelevandrei <32262415+pavelevandrei@users.noreply.github.com>
2022-05-27 12:10:15 +0300

    Поиск по эмбеддингу изображения в созданном индексе

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import numpy as np
 import os
 import faiss
 import streamlit
 from source.calculate_and_load_emb import EmbeddingCalculator
 
 ## cache?
+TOPN = 6
+
 ec = EmbeddingCalculator()
 
-#ec.calculate_emb(os.path.join("data", "images", "all", "1.jpg"))
-print(ec.calculate_emb(os.path.join("data", "images", "all", "1.jpg")))
+def load_index():
+    index = faiss.read_index(os.path.join("data", "indices", "flat.index"))
+    return index
 
-index = faiss.read_index(os.path.join("data", "indices", "flat.index"))
+index=load_index()
+
+search_vector = np.array(ec.calculate_emb(os.path.join("data", "images", "all", "1.jpg")), dtype='float32').reshape(1,-1)
+
+print(search_vector.shape)
+
+
+
+D, I = index.search(search_vector, TOPN)
+print(I)
