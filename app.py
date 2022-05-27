f649757b0ddea8f589f3f00e1e04c23487b23bd2
pavelevandrei <32262415+pavelevandrei@users.noreply.github.com>
2022-05-27 14:07:39 +0300

    Обновление requirements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import faiss
-import streamlit
+import streamlit as st
 from source.calculate_and_load_emb import EmbeddingCalculator
 
 ## cache?
@@ -9,11 +9,29 @@ TOPN = 6
 
 ec = EmbeddingCalculator()
 
+@st.cache
 def load_index():
     index = faiss.read_index(os.path.join("data", "indices", "flat.index"))
     return index
-
 index=load_index()
+
+st.title(f"Поиск похожих изображений для модерации сетевых сообществ")
+st.markdown(f"""
+Поиск похожих изображений в сообществах ВКонтакте **"Барахолки"** и **"Объявления"** .
+
+Используемый подход:
+* Собираем базу изображений
+* Присваиваем каждому изображению идентификатор
+* Рассчитываем векторное представление (эмбеддинг), используемая модель **resnet50**
+* Полученные векторные представления индексируем в эффективной структуре для поиска с помощью библиотеки **faiss**
+* Пользователь может загрузить изображение и найти похожие на него изображения в сообществах-барахолках
+*** 
+""")
+
+images_count = len(os.listdir(os.path.join("data", "images", "all")))
+
+image_number = st.number_input(label="Введите номер изображения", value=0, min_value=0, max_value=images_count-1, step=1)
+
 
 search_vector = np.array(ec.calculate_emb(os.path.join("data", "images", "all", "1.jpg")), dtype='float32').reshape(1,-1)
 
