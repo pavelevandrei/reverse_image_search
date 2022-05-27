46e78400a18e94c1701f375fd4ca9f6d9f3901ea
pavelevandrei <32262415+pavelevandrei@users.noreply.github.com>
2022-05-27 17:53:31 +0300

    Комментарии по индексу

--- source/build_index.py
+++ source/build_index.py
@@ -1,26 +1,34 @@
 import os
 import pickle
-
 import numpy as np
 import faiss
 
+
 TOPN = 6
-# owner_id = "78815338"
+FAISS_PARAMETERS_STRING = "IVF120,SQ8" # тип индекса
+
+# загружаем массив эмбеддингов
 emb_path = os.path.join("..", "data", "embeddings", f"all.pickle")
 with open(emb_path, 'rb') as file:
     embedding_list = pickle.load(file)
 emb_array = np.array(embedding_list, np.float32)
-norm = np.linalg.norm(emb_array)
-emb_array = emb_array / norm # нормализация
 
+# индентификаторы - порядок файла в папке
 ids = np.array(range(0, emb_array.shape[0]))
 ids = np.asarray(ids.astype('int64'))
 
-
-index = faiss.IndexIDMap(faiss.IndexFlatL2(emb_array.shape[1])) # IndexFlatIP для косинусного расстояния
+# создаём индекс
+index = faiss.index_factory(emb_array.shape[1], FAISS_PARAMETERS_STRING, faiss.METRIC_INNER_PRODUCT)
+index.train(emb_array)
 index.add_with_ids(emb_array, ids)
+
+# сохраняем индекс
+faiss.write_index(index, os.path.join("..", "data", "indices", "ivf.index"))
+
+# проверяем работоспособность
 print(index.ntotal)
-D, I = index.search(emb_array[5333:5338], TOPN)
+print(emb_array[0:1])
+D, I = index.search(emb_array[0:1], TOPN)
 print(I)
 print(D)
-faiss.write_index(index, os.path.join("..", "data", "indices","flat.index"))
+
