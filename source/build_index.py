a37a9ae796594f01ed97f5706ebf0e512a426109
pavelevandrei <32262415+pavelevandrei@users.noreply.github.com>
2022-05-27 10:36:19 +0300

    Строим индекс по всем эмбеддингам

--- source/build_index.py
+++ source/build_index.py
@@ -5,8 +5,8 @@ import numpy as np
 import faiss
 
 TOPN = 6
-owner_id = "78815338"
-emb_path = os.path.join("..", "data", "embeddings", f"{owner_id}.pickle")
+# owner_id = "78815338"
+emb_path = os.path.join("..", "data", "embeddings", f"all.pickle")
 with open(emb_path, 'rb') as file:
     embedding_list = pickle.load(file)
 emb_array = np.array(embedding_list, np.float32)
@@ -20,6 +20,6 @@ ids = np.asarray(ids.astype('int64'))
 index = index = faiss.IndexIDMap(faiss.IndexFlatL2(emb_array.shape[1])) # IndexFlatIP для косинусного расстояния
 index.add_with_ids(emb_array, ids)
 print(index.ntotal)
-D, I = index.search(emb_array[:5], TOPN)
+D, I = index.search(emb_array[5333:5338], TOPN)
 print(I)
 print(D)
